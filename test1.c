777e94a6708dc34c0b514a39adf81cf27e7b98dc
Dylan Moore <c3328302@uon.edu.au>
2019-04-09 03:17:10 +0000

    Encrypts text then prints it to file and console

--- test1.c
+++ test1.c
@@ -1,41 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 
-#define STR_LTH 1000                                //makes the length 25
+#define STR_LTH 26                                      //makes the length 25
 
-char encrypt(char *wrd);                            //function for encrypting 
+char encrypt(char *wrd);                                //function for encrypting 
 
 int main()
 {
-    char wrd[STR_LTH];                              //declares string 
-    FILE *TextToEncrypt;                            //sets a pointer to the file TextToEncrypt
-    TextToEncrypt=fopen("TextToEncrypt.txt","r");   //opens and reads the file
-//    FILE *EncryptedText;
-//    Encryptedtext=fopen("EncryptedText.txt","w");
-    while(!feof(TextToEncrypt))                     // makes a loop so that while the file is still going
+    char wrd[STR_LTH];                                  //declares string 
+    FILE *TextToEncrypt;                                //sets a pointer to the file TextToEncrypt
+    TextToEncrypt=fopen("TextToEncrypt.txt","r");       //opens and reads the file
+    FILE *EncryptedText;
+    EncryptedText=fopen("EncryptedText.txt","w");
+    while(!feof(TextToEncrypt))                         // makes a loop so that while the file is still going
     {
-        fscanf(TextToEncrypt,"%s",wrd);             //reads a word in the file then stores it in a string
-        encrypt(wrd);                               //calls function to that string
-        printf("%s ",wrd);                          //prints the encrypted text to the console
-//        fprintf("%s ",wrd); 
+        fscanf(TextToEncrypt,"%s",wrd);                 //reads a word in the file then stores it in a string
+        encrypt(wrd);                                   //calls function to that string
+        printf("%s ",wrd);                              //prints the encrypted text to the console
+        fprintf(EncryptedText, "%s ",wrd);              //prints encrypted text to the file EncryptedText
     }
-    printf("\n");                                   //prints new line to the console
-//    fprintf("\n");
-
+    printf("\n");                                       //prints new line to the console
+    fprintf(EncryptedText, "\n");                       //prints new line to file 
+    fclose(TextToEncrypt);                              //closes file
+    fclose(EncryptedText);                              //closes file
   return 0;
 }
 
 char encrypt(char *wrd)
 {
     
-    int i=0;                                        //declares i as an int
-    for(i = 0;i < STR_LTH;i++)                      //for thr length of the string 
+    int i=0;                                            //declares i as an int
+    for(i = 0;i < STR_LTH;i++)                          //for thr length of the string 
     {
-        if(wrd[i] == '\0')                          //if the word ends return 0
+        if(wrd[i] == '\0')                              //if the word ends return 0
         {
             return 0;
         }
-        wrd[i]=wrd[i]+3;                            //shifts char value up by an amount
+        wrd[i]=wrd[i]+3;                                //shifts char value up by an amount
     }
     return 0;
 }
