3c7a4ac1f0a2cfbccc8b88f6f049499a7e5c6f6d
Dylan Moore <c3328302@uon.edu.au>
2019-04-09 03:42:14 +0000

    Interactive Cypher

--- test1.c
+++ test1.c
@@ -3,30 +3,34 @@
 
 #define STR_LTH 26                                      //makes the length 25
 
-char encrypt(char *wrd);                                //function for encrypting 
+char encrypt(char *wrd, int key);                       //function for encrypting 
 
 int main()
 {
-    char wrd[STR_LTH];                                  //declares string 
+    char wrd[STR_LTH];                                  //declares string
+    int key;
     FILE *TextToEncrypt;                                //sets a pointer to the file TextToEncrypt
     TextToEncrypt=fopen("TextToEncrypt.txt","r");       //opens and reads the file
     FILE *EncryptedText;
     EncryptedText=fopen("EncryptedText.txt","w");
+    printf("Enter a value to rotate the letters by:\n");
+    scanf("%d",&key);    
+    
     while(!feof(TextToEncrypt))                         // makes a loop so that while the file is still going
     {
         fscanf(TextToEncrypt,"%s",wrd);                 //reads a word in the file then stores it in a string
-        encrypt(wrd);                                   //calls function to that string
+        encrypt(wrd, key);                             //calls function to that string       
         printf("%s ",wrd);                              //prints the encrypted text to the console
         fprintf(EncryptedText, "%s ",wrd);              //prints encrypted text to the file EncryptedText
     }
     printf("\n");                                       //prints new line to the console
-    fprintf(EncryptedText, "\n");                       //prints new line to file 
+    fprintf(EncryptedText, "\n");                       //prints new line to file
     fclose(TextToEncrypt);                              //closes file
     fclose(EncryptedText);                              //closes file
   return 0;
 }
 
-char encrypt(char *wrd)
+char encrypt(char *wrd, int key)
 {
     
     int i=0;                                            //declares i as an int
@@ -36,7 +40,7 @@ char encrypt(char *wrd)
         {
             return 0;
         }
-        wrd[i]=wrd[i]+3;                                //shifts char value up by an amount
+        wrd[i]=wrd[i]+key;                                //shifts char value up by an amount
     }
     return 0;
 }
