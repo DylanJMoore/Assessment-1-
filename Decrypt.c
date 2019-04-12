ef1431c448b00ad7aa63e2adfe3f8e12516a27fa
Dylan Moore <48810628+DylanJMoore@users.noreply.github.com>
2019-04-12 10:20:53 +1000

    Update Decrypt.c
    Decrypt with capital letters and overflow fix

--- Decrypt.c
+++ Decrypt.c
@@ -3,35 +3,39 @@
 
 #define STR_LTH 26                                      //makes the length 25
 
-char decrypt(char *wrd, int key);                       //function for decrypting 
+char decrypt(char *wrd, int key);                       //function for encrypting 
 
 int main()
 {
     char wrd[STR_LTH];                                  //declares string
     int key=0;
-    FILE *EncryptedText;                                //sets a pointer to the file EncryptedText
+    FILE *EncryptedText;                                //sets a pointer to the file TextToEncrypt
     EncryptedText=fopen("EncryptedText.txt","r");       //opens and reads the file
     FILE *DecryptedText;
     DecryptedText=fopen("DecryptedText.txt","w");
     printf("Enter the value of the rotation cypher:\n");
     scanf("%d",&key);    
-    if(key!=0)
+    if(key!='\0')
     {
     while(!feof(EncryptedText))                         // makes a loop so that while the file is still going
     {
         fscanf(EncryptedText,"%s",wrd);                 //reads a word in the file then stores it in a string
         decrypt(wrd, key);                             //calls function to that string       
-        printf("%s ",wrd);                              //prints the decrypted text to the console
-        fprintf(DecryptedText, "%s ",wrd);              //prints decrypted text to the file DecryptedText
+        printf("%s ",wrd);                              //prints the encrypted text to the console
+        fprintf(DecryptedText, "%s ",wrd);              //prints encrypted text to the file EncryptedText
     }
     printf("\n");                                       //prints new line to the console
     fprintf(DecryptedText, "\n");                       //prints new line to file
     fclose(EncryptedText);                              //closes file
     fclose(DecryptedText);                              //closes file
     }
-    else
+    else if(key=='\0')
     {
-        printf("Try again entering the cypher value again\n");
+        printf("Try again entering the cypher value though the terminal again\n");
+    }
+    else if(key==0)
+    {
+        printf("Enter a number other than zero\n");
     }
 
   return 0;
@@ -47,7 +51,17 @@ char decrypt(char *wrd, int key)
         {
             return 0;
         }
-        wrd[i]=wrd[i]-key;                                //shifts char value down by an amount specified
+        
+        wrd[i]=wrd[i]-key;                                //shifts char value up by an amount
+        
+        if(wrd[i]>90)
+        {
+            wrd[i]=wrd[i]-25;
+        }
+        else if(wrd[i]<65)
+        {
+            wrd[i]=wrd[i]+26;
+        }
     }
     return 0;
 }
